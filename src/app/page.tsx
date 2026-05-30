5ce382cb1bc1f083f98cc3b17409bd80416a9d94
ShubhOmKanchan <shubhom.k210104272@kccollege.edu.in>
2026-05-30 12:06:03 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -549,7 +549,7 @@ export default function Home() {
                   </p>
                 </div>
 
-                <div className="rounded-full border border-cyan-400/30 px-3 py-1 text-sm text-cyan-300">
+                <div className="mx-auto md:mx-0 w-fit md:w-auto rounded-full border border-cyan-400/30 px-4 py-2 text-sm text-cyan-300">
                   Automation • Analytics • AI
                 </div>
               </div>
@@ -1225,7 +1225,7 @@ export default function Home() {
                 </p>
 
                 <div className="flex h-full items-center">
-                  <h3 className="text-2xl md:text-3xl font-black">
+                  <h3 className="break-all text-base md:text-3xl font-bold text-white">
                     Connect Professionally
                   </h3>
                 </div>
